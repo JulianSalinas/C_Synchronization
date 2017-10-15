6f5249addf7f4b1b9352f23c6cb6f60ea3d0130e
Brandon <bdinarte@yahoo.com>
2017-10-14 21:08:32 -0700

    Productor
    Obtiene referencia, lee, suelta referencia

--- Headers/prod.h
+++ Headers/prod.h
@@ -1,4 +1,9 @@
 #ifndef C_SYNCHRONIZATION_PROD_H
 #define C_SYNCHRONIZATION_PROD_H
 
+#include "main.h"
+
+/* Function definitions */
+int prod_main(int argc, char *argv[]);
+
 #endif //C_SYNCHRONIZATION_PROD_H
--- Sources/prod.c
+++ Sources/prod.c
@@ -1,1 +1,50 @@
 #include "../Headers/prod.h"
+
+#define SHM_SIZE 1024  /* Kb de memoria compartida */
+
+int prod_main(int argc, char *argv[]) {
+    printf("Programa Productor \n");
+    printf("------------------ \n");
+
+    key_t key;
+    int shmid;
+    char *data;
+    int mode;
+
+    /* Key */
+    if ((key = ftok("key.txt", 'R')) == -1)
+    {
+        perror("Error de generacion de la clave. \n");
+        exit(1);
+    }
+
+    /* Reservar la memoria compartida */
+    /* shmid -> shared memory id */
+    if ((shmid = shmget(key, SHM_SIZE, 0644)) == -1) {
+        perror("Error de acceso a memoria. \n");
+        exit(1);
+    }
+
+    /****************************************************************************/
+
+
+    while(1) {
+        getchar();
+
+        /* attach to the segment to get a pointer to it: */
+        data = shmat(shmid, NULL, 0);
+        if (data == (char *)(-1)) {
+            perror("shmat");
+            exit(1);
+        }
+
+        printf("segment contains: \"%s\"\n", data);
+
+        /* detach from the segment: */
+        if (shmdt(data) == -1) {
+            perror("shmdt");
+            exit(1);
+        }
+    }
+
+}
