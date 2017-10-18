df706d29e3580629c0c012adb7493f10b3a05b86
Brandon <bdinarte@yahoo.com>
2017-10-18 15:53:50 -0700

    Arreglada la SHM, Comenzando el PROD

--- Sources/Util/smemory.c
+++ Sources/Util/smemory.c
@@ -4,25 +4,22 @@ void instance_MemorySimulation(int shm_id, int cell_amount){
 
     MCell * empty_cell = malloc(sizeof(MCell));
 
-    /* Inicializar celda vacia */
-    empty_cell->cell_number = -1;
+    /* Dar valores a celda, son negativos para saber que son
+     * de inicializacion y no ejecucion */ 
     empty_cell->held_proc_num = -1;
     empty_cell->held_proc_part = -1;
 
     /* Obtener puntero a memoria compartida */
     void * shm_address;
     shm_address = shmat(shm_id, NULL, 0);
-    printf("add: %d\n", shm_address);
-
-    if (shm_address < 0) {
-        perror("shmat");
-        exit(-1);
-    }
-
-    write_new_file(MEMADDRESS_FILENAME, (int) shm_address, 1);
 
     /* Copiar las celdas vacias a memoria compartida */
     for (int i = 0; i < cell_amount; i++){
+        
+        /* Identificador del numero de celda */
+        empty_cell->cell_number = i+1;
+        
+        /* Copiar la celda a la memoria compartida */
         memcpy(shm_address, empty_cell, sizeof(MCell));
         shm_address += sizeof(MCell);
     }
@@ -31,5 +28,4 @@ void instance_MemorySimulation(int shm_id, int cell_amount){
     shmdt(shm_address);
     free(empty_cell);
 
-    fin_main(0, NULL);
 }
--- Sources/prod.c
+++ Sources/prod.c
@@ -9,66 +9,66 @@ int prod_main(int argc, char *argv[]) {
 
     /* shared memory vars */
     key_t key;
-    int shmid;
-    char *data;
+    int shm_id;
+    MCell * mem_cell;
     int mem_size = read_file_int(MEMSIZE_FILENAME);
 
     /* semaphore vars */
-    sem_t *sem_des;
+    sem_t * sem_des;
 
     /* Key */
-    if ((key = ftok("key.txt", 'R')) == -1)
+    if ((key = ftok(KEY_FILENAME, 'R')) == -1)
     {
         perror("Error de generacion de la clave. \n");
-        exit(1);
+        exit(-1);
     }
 
     /* Obtener el ID de la memoria compartida */
-    if ((shmid = shmget(key, mem_size, 0644)) == -1) {
+    if ((shm_id = shmget(key, mem_size, 0644)) == -1) {
         perror("Error de acceso a memoria. \n");
-        exit(1);
+        exit(-1);
     }
 
     /****************************************************************************/
 
 
-    while(1) {
+    //while(1) {
         getchar();
 
         /* Open the Semaphore */
 
-        sem_des = sem_open(SEM_NAME, 0);
+        //sem_des = sem_open(SEM_NAME, 0);
 
-        if(sem_des == (void*) -1){
+        /*if(sem_des == (void*) -1){
             perror("sem_open failure");
             exit(1);
         }
 
         /* Lock the semaphore */
 
-        if(!sem_wait(sem_des)) {
+        //if(!sem_wait(sem_des)) {
 
             /* Access to the shared memory area */
             /* attach to the segment to get a pointer to it: */
-            data = shmat(shmid, NULL, 0);
-            if (data == (char *) (-1)) {
-                perror("shmat");
-                exit(1);
+            mem_cell = (MCell *) shmat(shm_id, NULL, 0);
+            if (mem_cell == (MCell *) (-1)) {
+                perror("Error de referencia a memoria.");
+                exit(-1);
             }
 
-            printf("segment contains: \"%s\"\n", data);
+            printf("Valor encontrado: %d \n", mem_cell->cell_number);
 
             /* detach from the segment: */
-            if (shmdt(data) == -1) {
-                perror("shmdt");
-                exit(1);
+            if (shmdt(mem_cell) == -1) {
+                perror("Error eliminando referencia a memoria.");
+                exit(-1);
             }
-        }
+        //}
         /* Release the semaphore lock */
 
-        sem_post(sem_des);
-        sem_close(sem_des);
+        //sem_post(sem_des);
+        //sem_close(sem_des);
 
-    }
+    //}
 
 }
